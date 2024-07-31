a79621e515569404e95b1f2da48379450ab6a6fd
Naomi <naomieisenstark@gmail.com>
2024-07-31 16:37:18 +0300

    comment in assembler controller

--- assembler/headers/assembler_controller.h
+++ assembler/headers/assembler_controller.h
@@ -1,4 +1,15 @@
 #ifndef ASSEMBLER_CONTROLLER_H
 #define ASSEMBLER_CONTROLLER_H
+/**
+ * Controller function to handle the execution of the assembler program.
+ *
+ * This function manages the workflow for processing each input file passed as command line arguments.
+ * It first preprocesses the files to handle macros, and then proceeds with the main processing phases.
+ * If any errors occur during processing, appropriate messages are displayed and processing may halt
+ * depending on the severity of the error.git
+ *
+ * @param argc The number of command line arguments.
+ * @param argv The array of command line arguments.
+ */
 void controller(int argc, char* argv[]);
 #endif /* ASSEMBLER_CONTROLLER_H */
--- assembler/sources/assembler_controller.c
+++ assembler/sources/assembler_controller.c
@@ -8,6 +8,17 @@
 /* ---------------------------------------------------------------------------------------
  *                                         Functions
  * --------------------------------------------------------------------------------------- */
+/**
+ * Controller function to handle the execution of the assembler program.
+ *
+ * This function manages the workflow for processing each input file passed as command line arguments.
+ * It first preprocesses the files to handle macros, and then proceeds with the main processing phases.
+ * If any errors occur during processing, appropriate messages are displayed and processing may halt
+ * depending on the severity of the error.
+ *
+ * @param argc The number of command line arguments.
+ * @param argv The array of command line arguments.
+ */
 void controller(int argc, char* argv[]) {
     int i = 1;            /* Index for iterating through loop */
     char* file_am = NULL; /* File name with .am extension */
