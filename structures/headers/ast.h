13e9a2d0929a50c06faaff95888a22cc893dbb1e
Naomi <naomieisenstark@gmail.com>
2024-11-28 11:52:25 +0200

    added comments

--- structures/headers/ast.h
+++ structures/headers/ast.h
@@ -148,4 +148,3 @@ Boolean add_directive_operand(Directive *directive, char *operand);
 void free_ast_node(ASTNode *node);
 
 #endif /* AST_H */
-/* commit */
--- utils/headers/boolean.h
+++ utils/headers/boolean.h
@@ -5,4 +5,3 @@
 typedef enum { FALSE, TRUE } Boolean;
 
 #endif /* BOOL_H */
-/* commit */
